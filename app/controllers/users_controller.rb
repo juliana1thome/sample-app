4a99b8b9f929f0ecc5e36427233758a165eb5eb9
juliana1thome <juju1thome@gmail.com>
2021-07-29 15:39:45 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
+    # If you want to debug use debugger. Don't forget that it needs the byebug gem
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    # @user = User.new(params[:user]) | Instead making the site super vunerable
+    # like this, wich will make possible any user become admin. We use a better
+    # version
+    @user = User.new(user_params)
+    if @user.save # Handle a successful save.
+      flash[:sucess] = "Welcome to my WONDERFUL Sample App ʕ•́ᴥ•̀ʔっ"
+      redirect_to @user
+    else
+      render 'new' # render the page new so the user can try again
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+    # the extra level of indentation on the
+    # user_params method is designed to make it visually
+    # apparent which methods are defined after private.
+  end
+
 end
